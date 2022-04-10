ec46d20d0e1a3f3b9548a4da351c9084496f518b
DomenicoCafarelli <94187765+DomenicoCafarelli@users.noreply.github.com>
2022-04-10 10:10:04 +0200

    Created views

---
+++ NFTGallery/NFTGallery/Views/SelectedCollection.swift
@@ -0,0 +1,20 @@
+//
+//  SelectedCollection.swift
+//  NFTGallery
+//
+//  Created by Domenico Cafarelli on 10/04/22.
+//
+
+import SwiftUI
+
+struct SelectedCollection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectedCollection_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectedCollection()
+    }
+}
---
+++ NFTGallery/NFTGallery/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  NFTGallery
+//
+//  Created by Domenico Cafarelli on 10/04/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
