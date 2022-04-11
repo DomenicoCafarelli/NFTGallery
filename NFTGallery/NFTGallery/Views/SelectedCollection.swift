4bc14eef8b60c745c595cde35085c89923cc96a5
DomenicoCafarelli <94187765+DomenicoCafarelli@users.noreply.github.com>
2022-04-11 17:04:35 +0200

    Added 3d models, added assets, created model data, data flow work

--- NFTGallery/NFTGallery/Views/SelectedCollection.swift
+++ NFTGallery/NFTGallery/Views/SelectedCollection.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct SelectedCollection: View {
+    
+    @StateObject var NFTstore = NFTStore()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Text(NFTstore.nft[0].NFT_name)
+        
     }
 }
 
